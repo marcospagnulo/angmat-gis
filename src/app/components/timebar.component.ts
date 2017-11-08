62e28c6cd0d8adff50e98692a736f23469586a21
marcospagnulo <marco.spagnulo@gmail.com>
2017-11-08 14:58:37 +0100

    timebar fix layout

--- src/app/components/timebar.component.ts
+++ src/app/components/timebar.component.ts
@@ -1,3 +1,4 @@
+import { NgIf } from '@angular/common/src/directives/ng_if';
 import { AppState } from '../app.state';
 import { Component, Input, ViewChild, AfterViewInit } from '@angular/core';
 import { select, NgRedux } from '@angular-redux/store';
@@ -8,7 +9,8 @@ import { Timebar } from '../store/timebar.reducer';
 @Component({
   selector: 'timebar',
   template: `
-    <mat-card *ngIf="( app.timebar.timebarLoading || app.timebar.timeslices?.length > 0 )">
+    <mat-card [ngClass]="{'scale-hide': !app.timebar.timebarLoading && app.timebar.timeslices?.length <= 0,
+                          'scale-show': app.timebar.timebarLoading || app.timebar.timeslices?.length > 0}">
 
       <!-- Loading -->
       <div *ngIf="app.timebar.timebarLoading" class="mat-spinner-container large-padding">
@@ -16,7 +18,7 @@ import { Timebar } from '../store/timebar.reducer';
       </div>
 
       <!-- Days -->
-      <mat-tab-group *ngIf="!app.timebar.timebarLoading" class="timebar-days">
+      <mat-tab-group *ngIf="!app.timebar.timebarLoading && app.timebar.timeslices?.length > 0" class="timebar-days">
         <mat-tab *ngFor="let day of timePerDay | mapKeys">
           <ng-template mat-tab-label>
             <span class="text subhead">{{day | ddMM}}</span>
@@ -32,7 +34,7 @@ import { Timebar } from '../store/timebar.reducer';
         </mat-tab>
       </mat-tab-group>
 
-      <div class="small-padding-top">
+      <div class="small-padding-top" *ngIf="app.selectedTimeslice !== null && !app.timebar.timebarLoading">
         <span class="text body">
           {{app.selectedTimeslice | dayOfWeek}} {{app.selectedTimeslice | ddMM}} {{app.selectedTimeslice | HHmm}}
         </span>
